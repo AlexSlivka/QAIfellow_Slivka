aad726eb77bdfb7b0db3330f7f4a249c427c838c
Slivka Alexey <alexslivka@mial.ru>
2024-11-22 22:11:28 +0300

    создана ветка IF_HW_3

--- src/main/java/ru/iFellow/Main.java
+++ src/main/java/ru/iFellow/Main.java
@@ -2,6 +2,6 @@ package ru.iFellow;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello IF_HW_3");
     }
 }
